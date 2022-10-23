3f91b9deff5e797bf513ec210e781dd3f468e2e7
Gokul <gokulr5102@gmail.com>
2022-10-23 07:47:21 +0530

    Ray casting algo

--- src/Maps.js
+++ src/Maps.js
@@ -116,15 +116,15 @@ function isMarkerInsidePolygon(poly) {
 
 function isPointInPoly(pt, poly) {
   poly = poly[0];
-  if()
-  for (var c = false, i = -1, l = poly.length, j = l - 1; ++i < l; j = i)
-    ((poly[i][1] <= pt[1] && pt[1] < poly[j][1]) ||
-      (poly[j][1] <= pt[1] && pt[1] < poly[i][1])) &&
-      pt[0] <
-        ((poly[j][0] - poly[i][0]) * (pt[1] - poly[i][1])) /
-          (poly[j][1] - poly[i][1]) +
-          poly[i][0] &&
-      (c = !c);
+  if (poly)
+    for (var c = false, i = -1, l = poly.length, j = l - 1; ++i < l; j = i)
+      ((poly[i][1] <= pt[1] && pt[1] < poly[j][1]) ||
+        (poly[j][1] <= pt[1] && pt[1] < poly[i][1])) &&
+        pt[0] <
+          ((poly[j][0] - poly[i][0]) * (pt[1] - poly[i][1])) /
+            (poly[j][1] - poly[i][1]) +
+            poly[i][0] &&
+        (c = !c);
   if (c) {
     swal({
       title: "Success",
